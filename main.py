d0cee2cfae3dee457bad72678f37389bd4286552
tahsinkh12e4 <138883367+tahsinkh12e4@users.noreply.github.com>
2023-07-07 18:51:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request
+from urllib.parse import urljoin
+import requests
+import re
+
+# Code Updated By (Mr Spider)
 
 app = Flask(__name__)
 
+headers = {
+    'Referer': 'https://pipcast.cc/'
+}
 
-@app.route('/')
+def get_base_url(url):
+    return urljoin(url, ".")
+
+@app.route("/")
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    channel_id = request.args.get("id")
+    
+    @app.route("/api/<string:channel_id>.m3u8")
+def handle_api(channel_id):
 
+    source_code = requests.get(f"https://pipcast.cc/embed.php?v={channel_id}&vw=100%&vh=100%").text
+    pattern = r"source:\s*['\"](.*?)['\"]"
+    match = re.search(pattern, source_code)
+    token_url = match.group(1)
+    response = requests.get(token_url, headers=headers)
+    lines = response.text.splitlines()
+    for index, line in enumerate(lines):
+        if ".ts" in line:
+            lines[index] = "/ts?id=" + line + "&base=" + get_base_url(get_base_url(token_url))      
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    return "\n".join(lines)
+
+@app.route("/ts")
+def handle_ts():
+    ts_id = request.args.get("id")
+    base = request.args.get("base")
+    response = requests.get(base + ts_id, headers=headers)
+    return response.content
+
+app.run(debug=True)
